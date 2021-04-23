af7bed537587e711c10ab7d74aae1335b688fff7
Steven <56651529+BigGucciSteve@users.noreply.github.com>
2021-04-22 17:57:53 -0700

    added the order history page and created a table to display the order history of the user

---
+++ Data/Models/OrderHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineRetailersStore.Data.Models
+{
+    public class OrderHistory
+    {
+        public String CustomerId { get; set; }
+        public String ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public String Name { get; set; }
+        public String OrderId { get; set; }
+
+        ICollection<Orders> Orders { get; set; }
+    }
+}
---
+++ Data/OrderHistoryService.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineRetailersStore.Data.Models;
+
+namespace OnlineRetailersStore.Data
+{
+    public class OrderHistoryService
+    {
+        public static List<OrderHistory> OrdersByCustomerId(string customerId)
+        {
+            List<OrderHistory> orders = new List<OrderHistory>();
+            using (var conn = new MySqlConnection(OnlineRetailersStoreContext.ConnectionString))
+            {
+                conn.OpenAsync();
+                var sql = $"SELECT * " +
+                    $"FROM online_retailers_store.order_history " +
+                    $"WHERE CustomerId = '{customerId}'";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        orders.Add(new OrderHistory()
+                        {
+                            CustomerId = reader["CustomerId"] as string,
+                            ProductId = reader["ProductId"] as string,
+                            Quantity = (int)(reader["Quantity"] as int?),
+                            Price = (decimal)(reader["Price"] as decimal?),
+                            Name = reader["Name"] as string,
+                            OrderId = reader["OrderId"] as string
+                        });
+                    }
+                }
+            }
+
+            return orders;
+        }
+
+    }
+}
