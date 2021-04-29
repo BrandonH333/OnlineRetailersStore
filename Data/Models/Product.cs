312a9078cddb2a0a1b71fa0aede5a6e519ae2cb3
vs256 <vs256@gmail.com>
2021-04-28 20:10:23 -0700

    Added product functionality

---
+++ Data/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineRetailersStore.Data.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string SupplierId { get; set; }
+        public string imageSrc { get; set; }
+    }
+}
---
+++ Data/Models/ProductList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace OnlineRetailersStore.Data.Models
+{
+    public class ProductList
+    {
+        public List<Product> list { get; set; } = new List<Product>();
+        public int size { get; set; }
+    }
+}
