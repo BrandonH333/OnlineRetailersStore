0dab573813771deac3485ce7fe79e0d4bfce87d6
vs256 <44126680+vs256@users.noreply.github.com>
2021-05-06 02:50:01 -0700

    Added shopping cart quantity button functionality, tables for product and shoppingcart update accordingly

--- Data/ShoppingCartService.cs
+++ Data/ShoppingCartService.cs
@@ -46,6 +46,50 @@ namespace OnlineRetailersStore.Data
             return shoppingCartList;
         }
 
+        public static bool UpdateQuantityForCart(string productId, int quantity, string userId)
+        {
+            using (var conn = new MySqlConnection(OnlineRetailersStoreContext.ConnectionString))
+            {
+                conn.OpenAsync();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = $"UPDATE online_retailers_store.shopping_cart " +
+                            $"SET Quantity = '{quantity}' " +
+                            $"WHERE ProductId = '{productId}' and UserId = '{userId}' ";
+                cmd.ExecuteNonQuery();
+            }
+            return true;
+        }
+
+        public static bool TakeAwayQuantityForProduct(int currentInventory, string productId, int quantity, string userId)
+        {
+            using (var conn = new MySqlConnection(OnlineRetailersStoreContext.ConnectionString))
+            {
+                conn.OpenAsync();
+                var cmd = conn.CreateCommand();
+
+                cmd.CommandText = $"UPDATE online_retailers_store.product " +
+                            $"SET Inventory = '{currentInventory - 1}' " +
+                            $"WHERE ProductId = '{productId}' ";
+                cmd.ExecuteNonQuery();
+            }
+            return true;
+        }
+
+        public static bool AddQuantityForProduct(int currentInventory, string productId, int quantity, string userId)
+        {
+            using (var conn = new MySqlConnection(OnlineRetailersStoreContext.ConnectionString))
+            {
+                conn.OpenAsync();
+                var cmd = conn.CreateCommand();
+
+                cmd.CommandText = $"UPDATE online_retailers_store.product " +
+                            $"SET Inventory = '{currentInventory + 1}' " +
+                            $"WHERE ProductId = '{productId}' ";
+                cmd.ExecuteNonQuery();
+            }
+            return true;
+        }
+
 
     }
 }
